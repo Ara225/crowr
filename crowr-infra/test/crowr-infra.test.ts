d299c4b7d1d1bef6dee088e0f70ecd3920c3b503
Anna Aitchison <anna11199810@hotmail.co.uk>
2022-06-21 19:57:25 +0100

    Bootstrap cdk

---
+++ crowr-infra/test/crowr-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrowrInfra from '../lib/crowr-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/crowr-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrowrInfra.CrowrInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
